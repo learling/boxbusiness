569c12d7836c2ebd5a24b1a13b16b055998584f4
ivannit <dev@ivanne.de>
2021-08-16 15:03:56 +0200

    Add post test

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-
+from django.contrib.auth.models import User, Group
+from django.conf import settings
 
 class TestViews(TestCase):
 
@@ -8,6 +9,7 @@ class TestViews(TestCase):
         self.client.login(username='test-user', password='test-pass')
 
     def setUp(self):
+        self.u = settings.TEST_USER
         self.client = Client()
         self.login_path = reverse('login')
         self.login_response = self.client.get(self.login_path)
@@ -33,3 +35,32 @@ class TestViews(TestCase):
         response = self.client.get(self.profile_path)
         expected_redirect = f'{self.login_path}?next={self.profile_path}'
         self.assertRedirects(response, expected_redirect)
+
+    def test_register_POST_valid(self):
+        response = self.client.post(reverse('register'), {
+            'username': self.u['name'],
+            'email': self.u['email'],
+            'password1': self.u['passw'],
+            'password2': self.u['passw']
+        })
+        user = User.objects.last();
+        self.assertEquals(user.username, self.u['name'])
+        group = Group.objects.get(name='customer')
+        self.assertEquals(len(user.groups.all()), 1)
+        self.assertEquals(user.groups.all()[0], group)
+        self.assertRedirects(response, self.login_path)
+
+    def test_register_POST_invalid(self):
+        pass
+
+    def test_register_POST_empty(self):
+        pass
+
+    def test_login_POST_valid(self):
+        pass
+
+    def test_login_POST_invalid(self):
+        pass
+
+    def test_login_POST_empty(self):
+        pass
