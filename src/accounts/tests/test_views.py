c85815307520235ed97bc82520ca01be26cbb32e
ivannit <dev@ivanne.de>
2021-08-16 14:23:11 +0200

    Add unit-tests

--- src/accounts/tests/test_views.py
+++ src/accounts/tests/test_views.py
@@ -14,22 +14,22 @@ class TestViews(TestCase):
         self.profile_path = reverse('profile')
         self.logout_path = reverse('logout')
 
-    def test_register_response_GET(self):
+    def test_register_GET(self):
         response = self.client.get(reverse('register'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/register.html')
 
-    def test_login_response_GET(self):
+    def test_login_GET(self):
         self.login_test_user()
         self.assertEquals(self.login_response.status_code, 200)
         self.assertTemplateUsed(self.login_response, 'accounts/login.html')
 
-    def test_logout_response(self):
+    def test_logout_GET(self):
         self.login_test_user()
         response = self.client.get(self.logout_path)
         self.assertRedirects(response, self.login_path)
 
-    def test_profile_response_GET(self):
+    def test_profile_GET(self):
         response = self.client.get(self.profile_path)
         expected_redirect = f'{self.login_path}?next={self.profile_path}'
         self.assertRedirects(response, expected_redirect)
