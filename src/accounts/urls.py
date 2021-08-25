d193f7845423bf7e0eb416cda093a06c5ff6c1f8
ivannit <dev@ivanne.de>
2021-08-25 10:39:57 +0200

    Begin reset password implementation

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+
 
 urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),  
     path('logout/', views.logout_user, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('users/', views.users, name='users')
+    path('users/', views.users, name='users'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name="accounts/password/reset.html"),
+        name="password_reset"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password/reset_sent.html"), 
+        name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password/reset_form.html"), 
+        name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password/reset_done.html"), 
+        name="password_reset_complete")
 ]
--- src/boxbusiness/settings.py
+++ src/boxbusiness/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Own config
+# Extra config
 
 TEST_USER = {
     'name': 'testingGoat',
@@ -167,3 +167,10 @@ SITE_OWNER = {
     'email': env('EMAIL'),
     'phone': env('PHONE')
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
