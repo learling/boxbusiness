42f95c82b29599c05ef85e7dc1dd379daf87de43
ivannit <dev@ivanne.de>
2021-08-26 09:55:09 +0200

    Add allowed host

--- src/boxbusiness/settings.py
+++ src/boxbusiness/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['dev.ivanne.de']
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
 if ALLOWED_HOSTS_ENV:
     ALLOWED_HOSTS.extend((ALLOWED_HOSTS_ENV).split(','))
