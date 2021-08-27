dc3ef0044a0fb25f7e23e82998850bd9082994f3
ivannit <dev@ivanne.de>
2021-08-27 14:53:03 +0200

    Try to test in container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,13 @@ set -e
 
 DJANGO_PORT=8000
 
-python manage.py collectstatic --noinput
+#TODO: Get testing work inside of docker
+##This is not working:
+#GV=v0.29.1
+#wget "https://github.com/mozilla/geckodriver/releases/download/$GV/geckodriver-$GV-linux64.tar.gz"
+#tar xvzf geckodriver-$GV-linux64.tar.gz
+#chmod +x geckodriver
+#sudo cp geckodriver /usr/local/bin/
 
+python manage.py collectstatic --noinput
 uwsgi --socket :$DJANGO_PORT --master --enable-threads --module boxbusiness.wsgi
